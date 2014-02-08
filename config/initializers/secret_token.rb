8c469bc7c0ea9a040c7fea17a2e36e7494550ca1
Danny Kirschner <rkirschner1377@gmail.com>
2014-02-07 22:58:36 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Musictown::Application.config.secret_key_base = '5d5cd83407b921332c9cd7e9014ce3684d2e61411ecebb759dd072944e155d1f7c8ae071260f4ef759c59d99b3ad950e116ee4bfdd383b91512e1d051be40f50'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Musictown::Application.config.session_store :cookie_store, key: '_musictown_session'
